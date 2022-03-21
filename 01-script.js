7b53635e12c29af7fc5c8f3a235cb6070b2291f4
Andrea <onemanshow2099@gmail.com>
2022-03-21 16:37:33 +0100

    001.1 valore opzioni

---
+++ 01-script.js
@@ -0,0 +1,9 @@
+let inizia = document.getElementById("bottone-difficolta");
+
+inizia.addEventListener("click",
+function() {
+    let difficolta = document.getElementById("difficolta-id").value;
+    console.log(difficolta) //abbiamo estratto il valore dalle opzioni
+}
+)
+
