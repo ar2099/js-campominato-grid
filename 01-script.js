5dc98919e27f6162ecb3c71e182ab20072a20c4a
Andrea <onemanshow2099@gmail.com>
2022-03-21 18:16:13 +0100

    continua

--- 01-script.js
+++ 01-script.js
@@ -1,9 +1,37 @@
 let inizia = document.getElementById("bottone-difficolta");
 
+
 inizia.addEventListener("click",
 function() {
-    let difficolta = document.getElementById("difficolta-id").value;
-    console.log(difficolta) //abbiamo estratto il valore dalle opzioni
+    let valueDifficolta = document.getElementById("difficolta-id").value;
+     //abbiamo estratto il valore dalle opzioni
+    
+    
+    
+    
+
+    if (valueDifficolta == 7){
+      difficolta = "difficile";
+        
+    }
+    else if (valueDifficolta == 9){
+      difficolta = "media";
+        
+    }
+    else {
+     difficolta = "facile";
+        
+    }
+    console.log(difficolta)
+
+    
 }
 )
 
+
+for (i = 1; i > 50; i++) {
+    let grid = document.getElementById("griglia");
+    let box = document.createElement("div");
+    grid.appendChild(box)
+
+}
